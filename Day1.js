0db16ca2f4b4cdb52766cc32b28c05a5e84fd463
MJ <mj632900@gmail.com>
2022-10-29 01:35:36 +0900

    Solve : Day1 All

---
+++ Day1.js
@@ -0,0 +1,20 @@
+// 두 수의 합
+function solution(num1, num2) {
+    return num1 + num2
+}
+
+// 두 수의 차
+function solution(num1, num2) {
+    return num1 - num2
+}
+
+// 두 수의 곱
+function solution(num1, num2) {
+    return num1 * num2
+}
+
+// 몫 구하기
+function solution(num1, num2) {
+    // Math.floor() 소수점 이하 버림
+    return Math.floor(num1 / num2);
+}
