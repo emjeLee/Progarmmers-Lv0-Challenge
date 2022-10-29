d7f2d59acf27671787b4592eb67c50e366f8c5eb
MJ <mj632900@gmail.com>
2022-10-30 01:32:03 +0900

    feat : while문 풀이 추가

--- Day2/Day2-4.js
+++ Day2/Day2-4.js
@@ -1,10 +1,23 @@
 // 배열 두배 만들기
+
+// for
 function solution(numbers) {
     const answer = [];
     for(let i=0; i<numbers.length; i++){
         answer.push(numbers[i] * 2)
     }
     return answer
+}
+
+// while
+function solution(numbers) {
+    const answer = [];
+    let count = 0;
+    while(count < numbers.length){
+        answer.push(numbers[count] * 2)
+        count += 1;
+    }
+    return answer;
 }
 
 // for문을 돌며 i번째 값에 2를 곱해준다
